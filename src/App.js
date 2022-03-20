cac8ec2ec8931ba39990be1da689ab51776c24a9
arifvtti <arif.vtti@gmail.com>
2022-03-20 10:28:13 +0600

    set counter in menubar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import Menubar from './components/Menubar/Menubar';
 // bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <Menubar></Menubar>
+      <Menubar count={count}></Menubar>
     </div>
   );
 }
--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Menubar.css"
-const Menubar = () => {
+const Menubar = (props) => {
     return (
         <div className='row my-3 container'>
             <div className='col-md-4'>
@@ -8,7 +8,7 @@ const Menubar = () => {
             </div>
             <div className="col-md-8 d-flex justify-content-end align-item-center">
                 <a href="/home">Home</a>
-                <a href="/cart">Cart</a>
+                <a href="/cart">Cart <sub>{props.count}</sub></a>
                 <a href="/about-us">About Us</a>
                 <a href="/account">Log in</a>
             </div>
